06ec47a8274437d2b07cb8b06743ce33f8c9b03b
gabrielthinassi <gabriel.m.thinassi@gmail.com>
2024-03-29 10:40:48 -0300

    feat: add filepurge

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Hangfire;
 using Hangfire.MemoryStorage;
+using simple_hangfire_schedule_net8.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,14 @@ app.UseHangfireDashboard();
 
 BackgroundJob.Enqueue(() => Console.WriteLine("Job rodou!"));
 
-RecurringJob.AddOrUpdate("MeuJobRecorrente", () =>  Console.WriteLine("Job Recorrente Rodou!"), Cron.Minutely, 
+RecurringJob.AddOrUpdate("MyRecurringJob", () =>  Console.WriteLine("Job Recorrente Rodou!"), Cron.Minutely, 
     new RecurringJobOptions { TimeZone = TimeZoneInfo.Local });
+
+RecurringJob.AddOrUpdate<FilePurge>(
+    "MyFilePurgeJob",
+    x => x.PurgeFromDirectory(@"C:\Dev\TESTES\temp"),
+    Cron.Daily,
+    new RecurringJobOptions { TimeZone = TimeZoneInfo.Local }
+);
 
 app.Run();
---
+++ Services/FilePurge.cs
@@ -0,0 +1,33 @@
+﻿namespace simple_hangfire_schedule_net8.Services
+{
+    public class FilePurge : IDisposable
+    {
+        public void Dispose()
+        { }
+
+        public void PurgeFromDirectory(string dirPath)
+        {
+            try
+            {
+                if (Directory.Exists(dirPath))
+                {
+                    string[] arquivos = Directory.GetFiles(dirPath);
+
+                    foreach (string arquivo in arquivos)
+                    {
+                        File.Delete(arquivo);
+                        Console.WriteLine($"Arquivo {arquivo} deletado com sucesso na pasta {dirPath}.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"A pasta {dirPath} não existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+    }
+}
